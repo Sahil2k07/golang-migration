52ccb2368732fa7ce83b39a09906e0d539a43172
shahil-resman <Shahil.Sharma@myresman.com>
2026-08-23 16:07:20 +0530

    Add migration summary logging
    Co-authored-by: Sahil2k07 <sahilsharma2k07@gmail.com>

--- internal/migrations/summary.go
+++ internal/migrations/summary.go
@@ -1,7 +1,21 @@
 package migrations
 
-import "time"
+import (
+	"log/slog"
+	"time"
+)
 
 func printMigrationSummary(start time.Time, table, index, view, function, procedure, trigger int) {
+	duration := time.Since(start)
 
+	slog.Info(
+		"Migration summary",
+		"duration", duration,
+		"tables", table,
+		"indexes", index,
+		"views", view,
+		"functions", function,
+		"procedures", procedure,
+		"triggers", trigger,
+	)
 }
