f8fbf5d211f8238efb53d899b47e19570257fe72
shahil-resman <Shahil.Sharma@myresman.com>
2026-08-24 16:08:50 +0530

    File logging implemented

--- internal/database/logger.go
+++ internal/database/logger.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"log"
 	"log/slog"
-	"os"
 	"time"
 
 	"github.com/Sahil2k07/golang-migration/internal/configs"
@@ -12,41 +11,29 @@ import (
 )
 
 type slogGormLogger struct {
-	logger *slog.Logger
+	gormLogger.Interface
 }
 
 func newGormLogger() gormLogger.Interface {
-	if configs.GetAppConfig().JSONLogs {
-		return &slogGormLogger{
-			logger: slog.Default(),
-		}
-	}
+	appConfigs := configs.GetAppConfig()
 
-	return gormLogger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags),
+	defaultLogger := gormLogger.New(
+		log.Default(),
 		gormLogger.Config{
 			SlowThreshold:             time.Second,
 			LogLevel:                  gormLogger.Info,
 			IgnoreRecordNotFoundError: true,
-			Colorful:                  true,
+			Colorful:                  !appConfigs.FileLogging,
 		},
 	)
-}
 
-func (l *slogGormLogger) LogMode(gormLogger.LogLevel) gormLogger.Interface {
-	return l
-}
+	if appConfigs.JSONLogs {
+		return &slogGormLogger{
+			Interface: defaultLogger,
+		}
+	}
 
-func (l *slogGormLogger) Info(ctx context.Context, msg string, data ...any) {
-	l.logger.InfoContext(ctx, msg, data...)
-}
-
-func (l *slogGormLogger) Warn(ctx context.Context, msg string, data ...any) {
-	l.logger.WarnContext(ctx, msg, data...)
-}
-
-func (l *slogGormLogger) Error(ctx context.Context, msg string, data ...any) {
-	l.logger.ErrorContext(ctx, msg, data...)
+	return defaultLogger
 }
 
 func (l *slogGormLogger) Trace(
@@ -64,11 +51,15 @@ func (l *slogGormLogger) Trace(
 	}
 
 	if err != nil {
-		l.logger.ErrorContext(ctx, "gorm query error", append(args, "error", err)...)
+		slog.ErrorContext(
+			ctx,
+			"GORM_ERROR",
+			append(args, "error", err)...,
+		)
 		return
 	}
 
 	if configs.IsDevelopment {
-		l.logger.InfoContext(ctx, "gorm query", args...)
+		slog.InfoContext(ctx, "GORM", args...)
 	}
 }
